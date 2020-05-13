a2f522eb6ddfe7eac1149fb385a82a28164080fc
pieceofr <komimi.p@gmail.com>
2020-05-13 12:51:57 +0800

    arrange CDSData struct order for better db viewing experience

--- parseCDS.go
+++ parseCDS.go
@@ -43,18 +43,19 @@ type CDSData struct {
 	State          string   `json:"state" bson:"state"`
 	Country        string   `json:"country" bson:"country"`
 	Level          string   `json:"level" bson:"level"`
-	CountryID      string   `json:"countryId" bson:"countryId"`
-	StateID        string   `json:"stateId" bson:"stateId"`
-	CountyID       string   `json:"countyId" bson:"countyId"`
-	Location       GeoJSON  `json:"location" bson:"location"`
-	Timezone       []string `json:"tz" bson:"tz"`
 	Cases          float64  `json:"cases" bson:"cases"`
 	Deaths         float64  `json:"deaths" bson:"deaths"`
 	Recovered      float64  `json:"recovered" bson:"recovered"`
 	ReportTime     int64    `json:"report_ts" bson:"report_ts"`
 	UpdateTime     int64    `json:"update" , bson:"update"`
 	ReportTimeDate string   `json:"report_date" bson:"report_date"`
+	CountryID      string   `json:"countryId" bson:"countryId"`
+	StateID        string   `json:"stateId" bson:"stateId"`
+	CountyID       string   `json:"countyId" bson:"countyId"`
+	Location       GeoJSON  `json:"location" bson:"location"`
+	Timezone       []string `json:"tz" bson:"tz"`
 }
+
 type GeoJSON struct {
 	Type        string    `bson:"type"`
 	Coordinates []float64 `bson:"coordinates"`
